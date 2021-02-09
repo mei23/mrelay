a9c97fe45dc40e5b50712b3dd83eb8a58a2e521b
mei23 <m@m544.net>
2021-02-09 13:46:10 +0900

    fix type

--- src/activitypub/type.ts
+++ src/activitypub/type.ts
@@ -1,21 +1,44 @@
 export type obj = { [x: string]: any };
 
-export type AsObject = IObject | string | (IObject | string)[];
+export type AsObject = IObject | Link | (IObject | Link)[];
+
+export type Link = string | ILink;
+
+export interface ILink {
+	type: 'Link';
+	href: string;
+	// TODO
+};
+
+export const isLink = (object: IObject): object is ILink => object.type === 'Link';
 
 export interface IObject {
 	'@context'?: string | obj | obj[];
 	type: string | string[];
 	id?: string;
-	published?: string;
+	published?: Date;
 	to?: AsObject;
 	cc?: AsObject;
 	attributedTo?: AsObject;
+	// TODO
+}
+
+export function extractType(object: IObject): string {
+	return typeof object.type === 'string' ? object.type : object.type[0];
+}
+
+export function extractId(x: IObject | Link): string {
+	const id = typeof x === 'string' ? x : isLink(x) ? x.href : x.id;
+	if (id == null) throw 'no id';
+	return id;
 }
 
 export interface IActivity extends IObject {
 	actor?: AsObject;
 	object?: AsObject;
 	target?: AsObject;
+
+	/* Linked Data Signatures */
 	signature?: {
 		type: string;
 		created: Date;
@@ -27,14 +50,18 @@ export interface IActivity extends IObject {
 }
 
 export interface IActor extends IObject {
+	// https://www.w3.org/TR/activitypub/#actor-objects
 	type: 'Person' | 'Service' | 'Organization' | 'Group' | 'Application';
-	inbox: IOrderedCollection | string;
-	outbox: IOrderedCollection | string;
-	following?: IOrderedCollection | ICollection | string;
-	followers?: IOrderedCollection | ICollection | string;
+	inbox: IOrderedCollection | Link;
+	outbox: IOrderedCollection | Link;
+	following?: IOrderedCollection | ICollection | Link;
+	followers?: IOrderedCollection | ICollection | Link;
 	preferredUsername?: string;
-	sharedInbox?: string;
+	endpoints?: {
+		sharedInbox?: IOrderedCollection | Link;
+	};
 
+	// https://docs.joinmastodon.org/spec/activitypub/#public-key
 	publicKey?: {
 		id?: string;
 		owner?: string;
@@ -45,41 +72,42 @@ export interface IActor extends IObject {
 export interface ICollection extends IObject {
 	type: 'Collection';
 	totalItems: number;
-	items?: AsObject;
-	current?: ICollectionPage;
-	first?: ICollectionPage;
-	last?: ICollectionPage;
+	items?: (IObject | Link)[];
+	current?: ICollectionPage | Link;
+	first?: ICollectionPage | Link;
+	last?: ICollectionPage | Link;
 }
 
 export interface ICollectionPage extends IObject {
 	type: 'CollectionPage';
 	totalItems: number;
-	items?: (IObject | string)[];
-	current?: ICollectionPage;
-	first?: ICollectionPage;
-	last?: ICollectionPage;	partOf: string;
-	next?: ICollectionPage;
-	prev?: ICollectionPage;
+	items?: (IObject | Link)[];
+	current?: ICollectionPage | Link;
+	first?: ICollectionPage | Link;
+	last?: ICollectionPage | Link;
+	partOf: ICollectionPage | Link;
+	next?: ICollectionPage | Link;
+	prev?: ICollectionPage | Link;
 }
 
 export interface IOrderedCollection extends IObject {
 	type: 'OrderedCollection';
 	totalItems: number;
-	orderedItems?: (IObject | string)[];
-	current?: IOrderedCollectionPage;
-	first?: IOrderedCollectionPage;
-	last?: IOrderedCollectionPage;
+	orderedItems?: (IObject | Link)[];
+	current?: IOrderedCollectionPage | Link;
+	first?: IOrderedCollectionPage | Link;
+	last?: IOrderedCollectionPage | Link;
 }
 
 export interface IOrderedCollectionPage extends IObject {
 	type: 'OrderedCollectionPage';
 	totalItems: number;
-	orderedItems?: (IObject | string)[];
-	current?: IOrderedCollectionPage;
-	first?: IOrderedCollectionPage;
-	last?: IOrderedCollectionPage;
-	partOf: string;
-	next?: IOrderedCollectionPage;
-	prev?: IOrderedCollectionPage;
+	orderedItems?: (IObject | Link)[];
+	current?: IOrderedCollectionPage | Link;
+	first?: IOrderedCollectionPage | Link;
+	last?: IOrderedCollectionPage | Link;
+	partOf: IOrderedCollectionPage | Link;
+	next?: IOrderedCollectionPage | Link;
+	prev?: IOrderedCollectionPage | Link;
 	startIndex?: number;
 }
